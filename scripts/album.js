829a73d2518fedcf3b5abd7f8b0461c3087aa04b
Amber Caldwell <acaldwell710@gmail.com>
2017-03-18 16:02:48 -0400

    Updated function, style, and structure to Albums

--- scripts/album.js
+++ scripts/album.js
@@ -1,33 +1,33 @@
 var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
  
  // Another Example Album
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
 
 // My Example Album
 var albumCoolJams = {
@@ -45,16 +45,17 @@ var albumCoolJams = {
     ]
 };
 
-var createSongRow = function (songNumber, songName, songLength) {
-    var template =
+ var createSongRow = function (songNumber, songName, songLength) {
+     var template =
         '<tr class="album-view-song-item">'
-        +   '   <td class="song-item-number>' + songNumber + '</td>'
-        +   '   <td class="song-item-title>' + songName + '</td>'
-        +   '   <td class="song-item-duration">' + songLength + '</td>'
-        +   '</tr>';
-    
-    return template;
-};
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
 
 // Must remain outside of function in order to be accessed by other functions
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -70,16 +71,23 @@ var setCurrentAlbum = function (album) {
         albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
         albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+     // initialize parent container
         albumSongList.innerHTML = '';
  
-     // #4
+     // call createSongRow & loop through all songs
         for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
  
- window.onload = function() {
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+window.onload = function() {
+
      setCurrentAlbum(albumPicasso);
  
      var myCurrentAlbums = [albumPicasso, albumMarconi, albumCoolJams];
@@ -91,6 +99,21 @@ var setCurrentAlbum = function (album) {
             i = 0;
         }
     });
+    
+    songListContainer.addEventListener('mouseover', function(event) {
+        // #1
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            // CODE IS WRONG -- BUT WHY??
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+    
+    for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
 };
 
 
